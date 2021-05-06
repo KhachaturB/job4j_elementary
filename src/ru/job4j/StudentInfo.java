d8e2928b6452ce68baee590000375bdcec3a35d3
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2021-05-06 16:54:41 +0300

    3.3. Разбор первой программы. [#159033 #665]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Khachatur Bagdasarian");
+        System.out.println("30.07.1989");
+    }
+}
