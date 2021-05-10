fe47323314ba85de28f7ce53d7fdc139622ea191
Khachatur <58373478+KhachaturB@users.noreply.github.com>
2021-05-10 11:59:23 +0300

    6.8. Дефрагментация массива [#225]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+public class Defragment {
+
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int notNullIndex = -1;
+                for (int i = index + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        notNullIndex = i;
+                        break;
+                    }
+                }
+                if (notNullIndex == -1) {
+                    break;
+                }
+                SwitchArray.swap(array, index, notNullIndex);
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
